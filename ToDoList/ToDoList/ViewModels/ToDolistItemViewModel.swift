8c49f487d383a14e5deea282e42b55df8761e7cc
hyonbokan <mkkanhb@gmail.com>
2023-10-10 14:44:26 +0900

    app icon, and launch screen

--- ToDoList/ToDoList/ViewModels/ToDolistItemViewModel.swift
+++ ToDoList/ToDoList/ViewModels/ToDolistItemViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by dnlab on 2023/10/05.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// ViewModel for a single to do list item view (each row in items list)
--- ToDoList/ToDoList/Views/ProfileView.swift
+++ ToDoList/ToDoList/Views/ProfileView.swift
@@ -13,10 +13,58 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    // Avatar
+                    profile(user: user)
+                } else {
+                    Text("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125 )
+            .padding()
+        // Info: Name, Email, Member since
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Member Since: ")
+                    .bold()
+                Text(
+                    "\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))"
+                )
+            }
+            .padding()
+        }
+        .padding()
+        // Log out button
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
     }
 }
 

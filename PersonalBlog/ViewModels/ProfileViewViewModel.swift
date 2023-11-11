2da94d27aebbb43cb93962a6a63f3a0f007084f4
hyonbokan <mkkanhb@gmail.com>
2023-11-11 21:14:13 +0900

    like toggle

--- PersonalBlog/ViewModels/ProfileViewViewModel.swift
+++ PersonalBlog/ViewModels/ProfileViewViewModel.swift
@@ -4,9 +4,41 @@
 //
 //  Created by dnlab on 11/9/23.
 //
-
+import FirebaseStorage
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel : ObservableObject {
+    @Published var currentUser: String = ""
     
+    init() {}
+    private let database = Firestore.firestore()
+    private let storage = Storage.storage().reference()
+    
+    public func getPosts(
+        username: String,
+        completion: @escaping (Result<[BlogPost], Error>) -> Void
+    ) {
+        let ref = database
+            .collection("users")
+            .document(username)
+            .collection("posts")
+        
+        ref.getDocuments { snapshot, error in
+            guard let posts = snapshot?.documents.compactMap({
+                BlogPost(with: $0.data()
+                )}).sorted(by: { return $0.date > $1.date
+                }),
+                  error == nil else {
+                return
+            }
+            completion(.success(posts))
+        }
+    }
+    
+    public func getUserProfileImageUrl(for username: String, completion: @escaping (URL?) -> Void) {
+        storage.child("\(username)/profile_picture.png").downloadURL { url, error in
+            completion(url)
+        }
+    }
 }
--- PersonalBlog/Views/CoreViews/ProfileView.swift
+++ PersonalBlog/Views/CoreViews/ProfileView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
+    @State private var blogPosts: [BlogPost] = []
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16)
+    ]
+    
     var body: some View {
-        Text("Profile view")
+        // Create top view for the profile pic and name
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 15) {
+                ForEach(blogPosts, id: \.id) { post in
+                    // Create View for the bottom
+                }
+            }
+            .onAppear{
+                viewModel.getPosts(username: "Akagi", completion: { result in
+                    switch result {
+                    case .success(let posts):
+                        self.blogPosts = posts
+                        print(posts)
+                    case .failure(let error):
+                        print(error)
+                    }
+                })
+            }
+        }
     }
 }
 
